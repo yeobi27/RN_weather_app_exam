2721623086e6cd59dd3bd6c60981b067e3e0eaa9
yeobi27 <yeobi27@gmail.com>
2025-05-09 17:00:49 +0900

    add plus minus

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [number, setNumber] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>결과 : {number}</Text>
+      <View style={{flexDirection: 'row'}}>
+        <Button 
+        title="증가"
+        onPress={() => {
+          setNumber(number + 1);
+        }}
+      />
+        <Button 
+          title="감소"
+          onPress={() => {
+            setNumber(number - 1);
+          }}
+      />
+      </View>
+      <StatusBar style="auto" />  // style: "auto" | "inverted" | "light" | "dark"
     </View>
   );
 }
@@ -16,5 +33,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',    
   },
 });
